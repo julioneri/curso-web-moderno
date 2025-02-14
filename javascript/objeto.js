d0108109066d87d43aed8152a6f437194a153d47
Julio Neri <160162554+julioneri@users.noreply.github.com>
2025-02-14 18:01:18 -0300

    feat(aula): criação e manipulação de objetos com propriedades dinâmicas em JavaScript

---
+++ javascript/objeto.js
@@ -0,0 +1,14 @@
+// const prod1 = new Object();
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4999.90
+prod1['Desconto Legal'] = 0.40 // "Permite" atributos com espaços (identificador como diciário)
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camsia Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
