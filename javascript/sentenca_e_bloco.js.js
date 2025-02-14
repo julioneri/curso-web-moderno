a16ab96dcad0517b9ca42d62cc4d68f79e65ade2
Julio Neri <160162554+julioneri@users.noreply.github.com>
2025-02-14 18:02:16 -0300

    feat(aula): impressão de números ímpares e pares com laços 'for' em JavaScript

---
+++ javascript/sentenca_e_bloco.js.js
@@ -0,0 +1,13 @@
+console.log("Bloco 1")
+for (let num = 1; num <= 10; num++) {
+    if (num % 2 == 1) {
+        console.log(num);
+    }
+}
+
+console.log("Bloco 2")
+for (let num = 1; num <= 10; num++) {
+    if (num % 2 == 0) {
+        console.log(num);
+    }
+}
