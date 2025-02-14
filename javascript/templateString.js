f17edfdb4a7569c5336e82d7ff28ec49b7caaa04
Julio Neri <160162554+julioneri@users.noreply.github.com>
2025-02-14 18:03:07 -0300

    feat(aula): concatenação e template literals em JavaScript

---
+++ javascript/templateString.js
@@ -0,0 +1,9 @@
+nome = "Rebecca";
+const contatenacao = "Olá, " + nome + "!";
+const template = `
+    Olá,
+    ${nome}!`;
+console.log(contatenacao, template);
+
+// Expressões...
+console.log(`1 + 1 = ${1+1}`);
